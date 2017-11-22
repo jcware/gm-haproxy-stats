f05bf9a4d7d924a96ec30efd7f50db4ecc061d91
julio Geomais <julio@geomais.com.br>
2017-11-22 18:27:21 -0200

    Using yarn and change the linter to standard

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,5 +1,27 @@
-import { expect } from 'chai';
+'option strict'
 
-describe('Main', () => {
+import chai, { expect } from 'chai'
+import { fetchCsv } from '../src/main'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import sinonStubPromise from 'sinon-stub-promise'
+chai.use(sinonChai)
+sinonStubPromise(sinon)
 
-});
+describe('Haroxy Stats', () => {
+
+  describe('Smoke Tests', () => {
+    it('Should HaproxyStats exist', () => {
+      expect(fetchCsv).to.exist
+    })
+    it('Should HaproxyStats have fetchCsv method', () => {
+      expect(fetchCsv).to.be.a('function')
+    })
+  })
+
+  describe('fetchCsv method', () => {
+    it('Should fetchCsv() return an array', () => {
+      expect(fetchCsv()).to.be.an('array')
+    })
+  })
+})
