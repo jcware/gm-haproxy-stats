8fe4cfcead8605c6cf0b8c0809ba2f1862ec9677
julio Geomais <julio@geomais.com.br>
2017-11-24 18:49:46 -0200

    add splitLines method

--- src/main.js
+++ src/main.js
@@ -1,18 +1,11 @@
 'use strict'
 
 const fetchCSV = (url, mode = {mode: 'no-cors'}) => {
-  fetch(
-            url,
-            mode)
+  fetch(url, mode)
           .then(data => data.text())
-          .then(data => parseCSV(data))
           .catch(err => err)
 }
 
-const parseCSV = (data) => {
-  let result = {}
-  let arr = data.split('\n')
-  console.log(arr[3])
-}
+const splitLines = () => {}
 
-export { fetchCSV }
+export { fetchCSV, splitLines }
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,15 +1,24 @@
 'use strict'
 
 import { expect } from 'chai'
-import { fetchCSV } from '../src/main'
+import { fetchCSV, splitLines } from '../src/main'
 
 global.fetch = require('node-fetch')
 // const haproxyUrl = 'http://producao.geomais.com.br:8088/admin?stats'
 
+// retrieve datacsv from haproxy stats url
+// split textDataCsv in lines
+// isolate header from backend
+// merge header with backends to contruct json object
+
 describe('Haroxy Stats', () => {
   describe('Smoke Tests', () => {
     it('Should HaproxyStats have fetchCSV method', () => {
-      expect(fetchCSV).to.be.a('function')
+      expect(fetchCSV).to.be.exist
+    })
+
+    it('Should HaproxyStats have splitLines method', () => {
+      expect(splitLines).to.be.exist
     })
   })
 
